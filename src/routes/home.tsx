ce233d439de7273240d85c2674e9f0d60f24fdec
KimJJRoSY <kjjeaus@gmail.com>
2023-09-10 18:26:47 +0900

    3.1 Post Tweet Form

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,19 @@
+import { styled } from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
 import { auth } from "../fbase";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
   const logOut = () => {
     auth.signOut();
   };
   return (
-    <h1>
-      <button onClick={logOut}> Log out</button>
-    </h1>
+    <Wrapper>
+      <PostTweetForm />
+      <h1>
+        <button onClick={logOut}> Log out</button>
+      </h1>
+    </Wrapper>
   );
 }
